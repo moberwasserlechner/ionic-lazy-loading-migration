4416586dd9552b0d1c78eaa8a5338b6bf16f87c0
Michael Oberwasserlechner <michael@byteowls.com>
2017-07-08 10:33:54 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# ionic-lazy-loading-migration
+# ionic-lazy-loading-migration [![Twitter Follow](https://img.shields.io/twitter/follow/michaelowl_web.svg?style=social&label=Follow&style=flat-square)](https://twitter.com/michaelowl_web)
+
 npm script helping to migrate a Ionic 2+ project to Ionic 3 lazy loading
+
+## Setup
+
+1. Copy the `make-lazy-script` folder to the root of you existing ionic 2+ project.
+2. add a script
+```
+  "scripts": {
+    "migrate-to-lazy": "node ./make-lazy-script/migrate-to-lazy-loading.js",
+  },
+```
+
+## Features
+
+* Create modules for pages in folders named `pages`
+** ngx-translate import is included
+* Remove unnecessary imports
+* Refactor page objects to strings
+`nav.setRoot('MyPage')` instead of `nav.setRoot(MyPage)` 
+* Create modules for components in folders named `components`
+
+## Customize
+
+The script uses template files with placeholders for generation and replacement. 
+
+E.g. After generating everything I discovered that I needed a custom pipe every page. 
+Knowing this before I would have added the import to my `page.module.[ngx-translate].tpl.txt`
+  
+* component.module.tpl.txt ... Module contents for components
+* page.module.ngx-translate.tpl.txt ... Module contents for pages with ngx-translate. Default.
+* page.module.tpl.txt ... Module contents for pages with ngx-translate (see arguments)
+* ionicpage.tpl.txt ... Adding `@Ionic()` and its import to pages
+
+If you like to use other folders than `pages` and `components` you have to change the variables controlling it.
+
+See https://github.com/git/git/blob/master/make-lazy-script/migrate-to-lazy-loading.js#L15-L16
+
+
+## Run it
+
+The script runs a simulation by default until you provide a cli argument and start it without simulation.
+
+```
+npm run tc:migrate-to-lazy
+```
+
+
+### CLI Arguments
+
+```
+npm run tc:migrate-to-lazy <PUT_THE_ARGUMENTS_HERE>
+```
+
+* dont-simulate ... Please be careful or use a scm to be able to revert changes :)
+* log-contents ... You will see the contents of created and changed files
+* exclude-pages ... Excludes generating page modules
+* exclude-page-cleanup ... Excludes generating clean up imports and replacing page references with strings
+* exclude-components ... Excludes generating custom component modules
+* no-ngx-translate ... If you do not want to include the ngx-translate module in every page module
+
+### Manual steps / known issues
+
+After running the script you have to check if everything was done correctly.
+
+In the cleanup step there are a few known issues because my regular expressions are generous. 
+So I you are fluent with RegExp and care to improve I gladly accept PRs.
+
+* Functions including the class name will be destroyed. I had functions like `toLoginPage()` resulting in `to'LoginPage'()`
+* String page references are quoted as well. So if you have `nav.setRoot('LoginPage')` this will result in `nav.setRoot(''LoginPage'')` 
+
+
---
+++ make-lazy-script/migrate-to-lazy-loading.js
@@ -0,0 +1,232 @@
+const fs = require('fs');
+const path = require('path');
+
+// flags
+let simulate = true;
+let logContents = false;
+let excludePages = false;
+let excludeComponents = false;
+let excludePageCleanUp = false;
+let noNgxTranslate = false;
+
+// ##############################################################################
+// this folders might exists anywhere in the directory structure
+
+let pagesFolder = "pages";
+let componentsFolder = "components";
+
+// ##############################################################################
+
+class ClassHolder {
+  constructor(name, path) {
+    this.name = name;
+    this.path = path;
+  }
+}
+
+// resolve cli arguments
+
+for (let val of process.argv) {
+  if (val === "dont-simulate")  {
+    simulate = false;
+  }
+  if (val === "log-contents")  {
+    logContents = true;
+  }
+
+  if (val === "exclude-pages")  {
+    excludePages = true;
+  }
+
+  if (val === "exclude-components")  {
+    excludeComponents = true;
+  }
+
+  if (val === "exclude-page-cleanup")  {
+    excludePageCleanUp = true;
+  }
+
+  if (val === "no-ngx-translate")  {
+    noNgxTranslate = true;
+  }
+}
+
+let files = readRecursive("./", null, "src");
+if (files.length > 0) {
+
+  let pageCnt = 0;
+  let componentCnt = 0;
+  let classes = [];
+
+  // templates
+  let pageModuleTemplate = fs.readFileSync(path.join(__dirname, "page.module.ngx-translate.tpl.txt"), "utf8");
+  // use does not have ngx translate
+  if (noNgxTranslate) {
+    pageModuleTemplate = fs.readFileSync(path.join(__dirname, "page.module.tpl.txt"), "utf8");
+  }
+  let componentModuleTemplate = fs.readFileSync(path.join(__dirname, "component.module.tpl.txt"), "utf8");
+  let ionicPageReplacement = fs.readFileSync(path.join(__dirname, "ionicpage.tpl.txt"), "utf8");
+
+  // regular expressions
+  const pagesRegExp = new RegExp(pagesFolder || "pages");
+  const componentsRegExp = new RegExp(componentsFolder || "components");
+  const classNameRegExp = /export class ([a-zA-Z]*?) .*{/;
+
+  for (let classPath of files) {
+    if (!excludePages) {
+      if (classPath.match(pagesRegExp) && classPath.match(/\.ts$/) && !classPath.match(/\.spec\.ts$/)) {
+
+        let basename = path.basename(classPath, ".ts");
+        let modulePath = path.join(path.dirname(classPath), basename + ".module.ts");
+
+
+        if (!fs.existsSync(modulePath)) {
+          // extract class name from file
+          let classContent = fs.readFileSync(classPath, "utf8");
+          const regexResult = classNameRegExp.exec(classContent);
+          if (regexResult && classContent.match(/@Component\(/)) {
+            let classname = regexResult[1];
+            if (classname) { // && !classname.match(/Page$/)
+              pageCnt++;
+              classname = classname.trim();
+              classes.push(new ClassHolder(classname, classPath));
+              console.log("\nPage class: " + classname);
+              // console.log("Page path: " + classPath);
+              // create module.ts
+              let moduleContent = pageModuleTemplate.replace(/LAZY-CLASSNAME-PLACEHOLDER/g, classname);
+              moduleContent = moduleContent.replace(/LAZY-FILENAME-PLACEHOLDER/g, basename);
+
+              if (logContents) {
+                console.log("Module path: " + modulePath);
+                console.log(moduleContent);
+              }
+              if (!simulate) {
+                // write module.ts file
+                fs.writeFileSync(modulePath, moduleContent, "utf8");
+              }
+
+              // update page controller
+              if (!classContent.match(/@IonicPage\(/)) {
+                classContent = classContent.replace(/@Component\({/, ionicPageReplacement);
+                if (logContents) {
+                  console.log(classContent);
+                }
+                if (!simulate) {
+                  fs.writeFileSync(classPath, classContent, "utf8");
+                }
+              } else {
+                console.warn("@IonicPage already included in "+classPath);
+              }
+              //
+              //break;
+            }
+          }
+        } else {
+          console.warn("Pages: '"+modulePath+"' already exists! No changes!");
+        }
+      } // pages
+    }
+
+    if (!excludeComponents) {
+      if (classPath.match(componentsRegExp) && classPath.match(/\.ts$/) && !classPath.match(/\.spec\.ts$/)) {
+        let basename = path.basename(classPath, ".ts");
+        let modulePath = path.join(path.dirname(classPath), basename + ".module.ts");
+
+        if (!fs.existsSync(modulePath)) {
+          // extract class name from file
+          let classContent = fs.readFileSync(classPath, "utf8");
+          const regexResult = classNameRegExp.exec(classContent);
+          if (regexResult && classContent.match(/@Component\(/)) {
+            let classname = regexResult[1];
+            if (classname) { // && !classname.match(/Page$/)
+              componentCnt++;
+              classname = classname.trim();
+              classes.push(new ClassHolder(classname, classPath));
+              console.log("\nComponent class: " + classname);
+              // console.log("Page path: " + classPath);
+              // create module.ts
+              let moduleContent = componentModuleTemplate.replace(/LAZY-CLASSNAME-PLACEHOLDER/g, classname);
+              moduleContent = moduleContent.replace(/LAZY-FILENAME-PLACEHOLDER/g, basename);
+
+              if (logContents) {
+                console.log("Component module path: " + modulePath);
+                console.log(moduleContent);
+              }
+              if (!simulate) {
+                // write module.ts file
+                fs.writeFileSync(modulePath, moduleContent, "utf8");
+              }
+            }
+          }
+        } else {
+          console.warn("Components: '"+modulePath+"' already exists! No changes!");
+        }
+      }
+    }
+
+  } //
+
+  console.log("\n");
+  if (!excludePages) {
+    console.log(pageCnt + " pages found!");
+  }
+  if (!excludeComponents) {
+    console.log(componentCnt + " components found!");
+  }
+
+  if (!excludePageCleanUp) {
+    for (let f of files) {
+      if (f.match(/\.ts$/) && !f.match(/\.spec\.ts$/)) {
+        let original = fs.readFileSync(f, "utf8");
+        let replaced = original;
+
+        for (let classHolder of classes) {
+          if (f === classHolder.path) {
+            // console.debug("File of class "+classHolder.name+" will be skipped");
+            continue;
+          }
+          replaced = replaced.replace(new RegExp("^.*import {"+classHolder.name+"} from .*$", 'mg'), "");
+          replaced = replaced.replace(new RegExp(classHolder.name, 'g'), "'"+classHolder.name+"'");
+        }
+
+        if (original !== replaced) {
+          if (logContents) {
+            console.log(replaced);
+          }
+
+          if (!simulate) {
+            fs.writeFileSync(f, replaced, "utf8");
+          } else {
+
+          }
+        }
+      }
+    }
+  }
+}
+
+// based on https://github.com/fs-utils/fs-readdir-recursive
+function readRecursive(root, files, prefix) {
+  prefix = prefix || '';
+  files = files || [];
+
+  let dir = path.join(root, prefix);
+  if (!fs.existsSync(dir)) {
+    return files
+  }
+  if (fs.statSync(dir).isDirectory()) {
+    fs.readdirSync(dir)
+      .filter(noDotFiles)
+      .forEach((name) => {
+        readRecursive(root, files, path.join(prefix, name))
+      });
+  } else {
+    files.push(prefix);
+  }
+
+  return files
+}
+
+function noDotFiles(x) {
+  return x[0] !== '.'
+}
